b65c44812237e610ec6b008b89fc2eac6ff57e29
linbetklin <linbetklin@163.com>
2024-02-01 01:24:02 +0800

    fix

--- index.php
+++ index.php
@@ -8,8 +8,9 @@
 		<meta name="description" content="Minecraft Rscgwj Server" />
 		<meta name="keywords" content="optifine, minecraft, fps, lag, antialiasing, hd textures, optifine下载, 我的世界高清优化补丁, mc高清优化补丁, 我的世界optifine, 我的世界光影, 光影补丁" />
 		<title>Minecraft Rscgwj Server</title>
-		<!-- <link rel="stylesheet" href="https://cdn.zerodream.net/css/bootstrap.min.css">
-		<link rel="stylesheet" href="https://cdn.zerodream.net/css/optifine.cn.css?s=1"> -->
+		<link rel="stylesheet" href="https://cdn.zerodream.net/css/bootstrap.min.css">
+		<!-- <link rel="stylesheet" href="https://cdn.zerodream.net/css/optifine.cn.css?s=1"> -->
+	<link rel="stylesheet"  type="text/css" href="./css/rscg.css"/>
 	</head>
 	<body>
 		<div class="container">
@@ -17,21 +18,22 @@
 				<div class="col-sm-1"></div>
 				<div class="col-sm-10 mainbox">
 					<div class="header-logo">
-						<h2 class="logo">Rscgwj</h2>
-						<div class="desc">MINECRAFT Rscgwj生存</div>
+						<h2 class="logo">RSCG</h2>
+						<div class="desc">MINECRAFT RSCG生存</div>
 					</div>
 					<div class="menu" nowrap>
 						<a href="/home" class="item">首页</a>
 						<a href="/downloads" class="item">下载</a>
-						<a href="https://optifine.net/donate" target="_blank" class="item">赞助</a>
+						<!-- <a href="https://optifine.net/donate" target="_blank" class="item">赞助</a>
 						<a href="/capeChange" class="item">披风</a>
 						<a href="/faq" class="item">FAQ</a>
 						<a href="/comments" class="item">讨论</a>
-						<a href="/apiDocs" class="item">API</a>
+						<a href="/apiDocs" class="item">API</a> -->
 					</div>
-					<div class="content"><p>OptiFine 是 Minecraft 优化 Mod，它支持高清纹理和许多配置选项，从而使 Minecraft 可以运行得更快，体验更好。</p>
+					<div class="content">
+						<!-- <p>OptiFine 是 Minecraft 优化 Mod，它支持高清纹理和许多配置选项，从而使 Minecraft 可以运行得更快，体验更好。</p>
 <p>OptiFine 的官方描述在 <a href="http://www.minecraftforum.net/topic/249637-" target="_blank">Minecraft 论坛</a> 上，以及一些相关资源：<a href="https://github.com/sp614x/optifine/tree/master/OptiFineDoc/assets/minecraft/optifine/lang" target="_blank">翻译</a>，<a href="https://github.com/sp614x/optifine/tree/master/OptiFineDoc/doc" target="_blank">文档</a>，<a href="https://github.com/sp614x/optifine/issues" target="_blank">问题跟踪器</a>。</p>
-<p>本站是 OptiFine 非官方中文介绍网站，由爱好者自行建立，英文官网请访问 <a href="https://optifine.net/" target="_blank">optifine.net</a></p>
+<p>本站是 OptiFine 非官方中文介绍网站，由爱好者自行建立，英文官网请访问 <a href="https://optifine.net/" target="_blank">optifine.net</a></p> -->
 <hr />
 </ul></div>
 					<div class="content-footer">
@@ -43,6 +45,6 @@
 			</div>
 		</div>
 	</body>
-	<!-- <script src="https://cdn.zerodream.net/js/jquery.min.js"></script>
-	<script src="https://cdn.zerodream.net/js/bootstrap.min.js"></script> -->
+	<script src="https://cdn.zerodream.net/js/jquery.min.js"></script>
+	<script src="https://cdn.zerodream.net/js/bootstrap.min.js"></script>
 </html>
